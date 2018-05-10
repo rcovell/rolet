eae3d8b9083a79338f7d054ba1deb21bb46ba681
Bob Covell <rcovell@rolet.com>
2018-05-10 13:50:43 -0500

    Added Cache-Control

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     </script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Cache-control" content="public">
     {{-- <meta name="viewport" content="width=device-width, initial-scale=1"> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     @hasSection('description')
